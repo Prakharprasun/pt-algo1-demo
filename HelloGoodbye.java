918b7ef6a2730dc58c022239fa797d1a1bd20edc
Prakharprasun <prakharprasun004@gmail.com>
2023-10-14 17:54:29 +0530

    Example of user input

---
+++ HelloGoodbye.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class HelloGoodbye{
+public static void main(String [] args){
+    var con = System.console();
+    if (con != null) {
+       
+        Scanner sc = new Scanner(con.reader());
+        
+        System.out.println("Enter name1");
+        String x = sc.nextLine();
+        System.out.println("Enter name2");
+        String y = sc.nextLine();
+        System.out.println("Hello "+x+"and "+y);
+        System.out.print("Goodbye "+y+"and "+x);
+    
+}}}
